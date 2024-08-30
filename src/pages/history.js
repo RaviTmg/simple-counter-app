c8271920fb87c81c3d0efafdb82ab382c03aa29c
ravitmg <tmgrawv@gmail.com>
2024-08-30 16:45:29 +0545

    show history only when non empty

--- src/pages/history.js
+++ src/pages/history.js
@@ -8,6 +8,9 @@ function HistoryItem ({ count }) {
 }
 
 export default function History ({ history }) {
+    if (!history.length) {
+        return <></>
+    }
     return (
         <div>
           <h1>History</h1>
